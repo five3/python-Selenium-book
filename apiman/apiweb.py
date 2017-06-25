6c40b4c079bcf44b78c1a0384d7e903ab808f418
Xiaowu Chen <five3@163.com>
2017-06-25 23:34:57 +0800

    web init

--- apiman/apiweb.py
+++ apiman/apiweb.py
@@ -1,17 +1,22 @@
 from flask import Flask
+from flask import render_template
 from flask_pymongo import PyMongo
-import json
 
 app = Flask(__name__)
-app.config['MONGO_HOST'] = '127.0.0.1'
-app.config['MONGO_PORT'] = 27017
 app.config['MONGO_DBNAME'] = 'apiman'
 mongo = PyMongo(app)
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def index():
-    active_user = mongo.db.users.find({'status': 'active'})
-    return json.dumps(active_user)
+    return render_template('index.html')
+
+@app.route('/testing', methods=['POST'])
+def testing():
+    return ""
+
+@app.route('/status')
+def status():
+    return render_template('status.html')
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=8000)
